aeb0161ac9d378d9e6f6578ccef8e9a6049ba7ce
Vi <vikamatuchko772@gmail.com>
2023-04-09 17:28:54 +0300

    can type numbers

--- dist/js/index.25e9331a0f7bdf6fc21a.js
+++ dist/js/index.25e9331a0f7bdf6fc21a.js
@@ -22,7 +22,7 @@ __webpack_require__.r(__webpack_exports__);
 
 const numBtns = document.querySelectorAll(".js-btn-num");
 const operantBtns = document.querySelectorAll(".js-btn-operant");
-// const funcBtns = document.querySelectorAll(".js-btn-func");
+const funcBtns = document.querySelectorAll(".js-btn-func");
 
 const fieldTotal = document.querySelector(".calc__total");
 const fieldHistory = document.querySelector(".calc__history");
@@ -31,43 +31,66 @@ const btnDelOne = document.querySelector(".js-btn-del-last");
 const btnPercent = document.querySelector(".js-btn-percent");
 const btnDeleteAll = document.querySelector(".js-btn-delete");
 
+const pointBtn = document.querySelector("#btn-point");
+
 const equalBtn = document.querySelector("#js-equal");
 
-class Calculator {
-  constructor(fieldTotal, fieldHistory) {
-    this.fieldHistory = fieldHistory;
-    this.fieldTotal = fieldTotal;
-    this.clear();
-  }
+// class Calculator {
+//   constructor(fieldTotal, fieldHistory) {
+//     this.fieldHistory = fieldHistory;
+//     this.fieldTotal = fieldTotal;
+//     this.clear();
+//   }
 
-  clear() {
-    this.currentOperand = "";
-    this.previousOperand = "";
-    this.operation = undefined;
-  }
+//   clear() {
+//     this.currentOperand = "";
+//     this.previousOperand = "";
+//     this.operation = undefined;
+//   }
 
-  delete() {}
+//   delete() {}
 
-  appendNumber(number) {}
+//   appendNumber(number) {}
 
-  chooseOperation(operation) {}
+//   chooseOperation(operation) {}
 
-  compute() {}
+//   compute() {}
 
-  updateDisplay() {}
-}
+//   updateDisplay() {}
+// }
 
-// numBtns.forEach((btn) => {
-//   btn.addEventListener("click", () => {
-//     fieldTotal.innerHTML += btn.innerHTML;
-//   });
-// });
+window.addEventListener("load", (e) => {
+  // localStorage.clear()
+  localStorage.setItem("initialVal", 0);
+  fieldTotal.innerHTML = localStorage.getItem("initialVal");
+  fieldHistory.classList.add("hidden");
+});
 
-// operantBtns.forEach((btn) => {
-//   btn.addEventListener("click", () => {
-//     fieldTotal.innerHTML !== "" && (fieldTotal.innerHTML += btn.innerHTML);
-//   });
-// });
+numBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (fieldTotal.innerHTML === "0") {
+      if (btn === pointBtn) {
+        fieldTotal.innerHTML += btn.innerHTML;
+      } else {
+        fieldTotal.innerHTML = btn.innerHTML;
+      }
+    } else {
+      fieldTotal.innerHTML += btn.innerHTML;
+    }
+  });
+});
+
+operantBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    fieldTotal.innerHTML !== "" && (fieldTotal.innerHTML += btn.innerHTML);
+  });
+
+  // if (fieldTotal.innerHTML.endsWith(btn)) {
+  //   fieldTotal.innerHTML += "";
+  //   console.log(1);
+  //   console.log(fieldTotal.innerHTML);
+  // }
+});
 
 // btnDeleteAll.addEventListener("click", () => {
 //   fieldTotal.innerHTML = "";
